7ec8817a9fb3c0ffebe70903283570803a2dbb40
julivansoares <julivan.soares@outlook.com>
2023-11-27 18:59:53 -0300

    iniciando dados sobre

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -4,16 +4,22 @@ import style from './style.module.scss'
 export default function AboutMe(){
 
     return(
-        <main className={style.main}> 
-         
-         <h1>julivan santos</h1>
-         
-         <h1>julivan santos</h1>
-         
-         <h1>julivan santos</h1>
-         
-         <h1>julivan santos</h1>
-        
+        <main className={style.main}>
+            <article className={style.sobre}>
+                <div class="titulo">
+                    <h1>Sobre Mim</h1>
+                    <p>Perfil Profissional - Tudo sobre mim</p>
+
+                    <div aria-label="Ilustração de botões">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div className={style.foto_pefil}>
+                  <img src="./public/img/Julivan_soares.png" alt="foto-pefil"></img>
+                </div>                
+         </article>
         </main>
   
     )
